090096d36aa6ace15e9a96614da3b0c3d5628f43
Gerardo Quintanar <gquintanar@rccl.com>
2019-01-07 02:04:54 -0500

    Main classes.

---
+++ InstagramTest/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  InstagramTest
+//
+//  Created by Gerardo on 1/7/19.
+//  Copyright © 2019 Gerardo Quintanar. All rights reserved.
+//
+
+import Foundation
+
+struct INSTAGRAM_CONSTANTS
+{
+    static let AUTHURL = "https://api.instagram.com/oauth/authorize/"
+    static let APIURL = "https://api.instagram.com/v1/users/"
+    static let CLIENTID = "fa37ebccb4144cc581e2e9c6d11af402"
+    static let CLIENTSECRET = "9e924fc442ce4f88a2f562d58bae89e9"
+    static let REDIRECTURL = "http://instagramtest.gerardoqm.com"
+    static let ACCESSTOKEN = "access_token"
+    static let SCOPE = "likes+comments+relationships"
+}
---
+++ InstagramTest/LoginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  LoginViewController.swift
+//  InstagramTest
+//
+//  Created by Gerardo on 1/7/19.
+//  Copyright © 2019 Gerardo Quintanar. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet weak var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func signInRequest() {
+        let url = String(format: "%@?client_id=%@&redirect_uri=%@&response_type=token&scope=%@&DEBUG=True", arguments: [INSTAGRAM_CONSTANTS.AUTHURL, INSTAGRAM_CONSTANTS.CLIENTID, INSTAGRAM_CONSTANTS.REDIRECTURL, INSTAGRAM_CONSTANTS.SCOPE])
+        let request = URLRequest.init(url: URL.init(string: url)!)
+        webView.loadRequest(request)
+        
+    }
+    func checkRequestForCallBackURL(request: URLRequest) -> Bool {
+        let urlString = (request.url?.absoluteString)! as String
+        if urlString.hasPrefix(INSTAGRAM_CONSTANTS.REDIRECTURL) {
+            let range : Range <String.Index> = urlString.range(of: "#access_token")!
+            getAuthTokenn(authToken: urlString.substring(from: range.upperBound))
+            return false
+        }
+        return true
+    }
+    func getAuthTokenn(authToken: String) {
+        let url = String(format: "https://api.instagram.com/v1/users/self/?access_token=%@")
+        let request: NSMutableURLRequest = NSMutableURLRequest(url: URL(string: url)!)
+        request.httpMethod = "GET"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        
+        session.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
+            if let data = data
+            {
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
+                let strFullName = (json?.value(forKey: "data") as AnyObject).value(forKey: "full_name") as? String
+                let alert = UIAlertController(title: "FULL_NAME", message: strFullName, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: NSLocalizedString("OK", comment: "OK action"), style: .default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert ,animated: true){ _ in }
+            }
+            
+        }.resume()
+    }
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool        <#function body#>
+    }
+}
